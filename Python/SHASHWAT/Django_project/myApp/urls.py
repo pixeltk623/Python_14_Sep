3dca5b2d46b9f9a337660da72c6b285bee9d204a
SHASHWAT GUPTA <shashwatgupta591@gmail.com>
2021-12-17 19:40:08 +0530

    crud done

--- Python/SHASHWAT/Django_project/myApp/urls.py
+++ Python/SHASHWAT/Django_project/myApp/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('update/', views.update, name='update'),
+    path('store/', views.store, name='store'),
+    path('show/<int:id>/',views.show, name='show'),
+    path('delete/<int:id>/',views.delete,name='delete'),
 ]
--- Python/SHASHWAT/Django_project/myApp/views.py
+++ Python/SHASHWAT/Django_project/myApp/views.py
@@ -1,8 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Crud
+from django.shortcuts import redirect  
+from django.contrib import messages
+import datetime
+import pytz
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request,'index.html')
+	all_entries = Crud.objects.all()
+	print(all_entries)
+	return render(request, 'index.html', {'all_entries' : all_entries})
 
+def create(request):
+    return render(request,'create.html')
+
+def store(request):
+	# return HttpResponse("STORE")
+
+	if request.method=='POST':
+		Name = request.POST['name']
+		Email = request.POST['email']
+		Mobile = request.POST['mobile']
+		gender = request.POST['gender']
+		hobby = request.POST.getlist('hobbies')
+		hobby = ','.join(hobby)
+	
+		cityName = request.POST['city']
+		res = Crud(name=Name, email=Email, mobile = Mobile, gender=gender, hobbies=hobby, cityName=cityName)
+		res.save()
+	messages.add_message(request, messages.INFO, 'New User Added')
+	return redirect('/')  
+
+def edit(request, id):
+	single_data = Crud.objects.get(pk=id)
+	return render(request, 'edit.html',{'single_data' : single_data})
+
+def update(request):
+	b = Crud.objects.get(pk=request.POST['uid'])
+	b.name = request.POST['name']
+	b.email = request.POST['email']
+	b.mobile = request.POST['mobile']
+	b.modified_date = datetime.datetime.now()
+	b.save()
+	messages.add_message(request, messages.INFO, 'User Updated')
+	return redirect('/')    
+
+def show(request,id):
+	single_data = Crud.objects.get(pk=id)
+	return render(request, 'show.html',{'single_data' : single_data})
+
+def delete(request, id):
+	b = Crud.objects.get(pk=id)
+	b.delete()
+	messages.add_message(request, messages.INFO, 'User Deleted')
+	return redirect('/')
